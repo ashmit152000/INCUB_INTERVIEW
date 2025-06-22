5c2ff264661ee2e086ee618bf1505fb0a9eb17d3
Ashmit Kashyap <ashmitkashyap@192.168.1.15>
2025-06-22 19:59:43 +0530

    Added all the cases and scenario for addition without negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe StringCalculator do
   it "should return 0 on empty String" do
     expect(StringCalculator.add("")).to  eq(0)
   end
+
+  it "should add two numbers after ," do
+    expect(StringCalculator.add("1,2")).to eq(3)
+  end
+
+  it "should add all numbers after ," do
+    expect(StringCalculator.add("1,2,3")).to eq(6)
+  end
+
+  it "should add all numbers after \n as delimiter" do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
+
+  it "should also add all the numbers in the case of custom delimiter" do
+      expect(StringCalculator.add("//;\n2;3")).to eq(5)
+  end
+
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,23 @@ class StringCalculator
 
   def self.add(numbers)
     return 0 if numbers.strip.empty?
+
+    delimiter = /,|\n/
+
+  # case for the custom delimiter
+  if numbers.start_with?("//")
+    customer_delimiter_value, numbers = numbers.split("\n") # split the delimiter and the numbers
+    customer_delimiter_array = customer_delimiter_value.split("") # split the delimiter part into all it's characters
+    delimiter = customer_delimiter_array[2] # fetch the custom delimiter and set it to delimiter variable
   end
 
+  numbers_array = numbers.split(delimiter).map(&:to_i) # split the numbers and convert into integer
+
+  numbers_array.sum
+  end
+
+  puts StringCalculator.add("1")
+  puts StringCalculator.add("1,2")
+  puts StringCalculator.add("1,2,3")
+  puts StringCalculator.add("1\n2,3")
 end
